d1c203b28a849e1e15096408a4583f1c7d3ac6c7
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-04-21 13:03:29 +0300

    migration fix

--- database/migrations/2020_04_05_214605_create_domains_table.php
+++ database/migrations/2020_04_05_214605_create_domains_table.php
@@ -14,7 +14,7 @@ class CreateDomainsTable extends Migration
     public function up()
     {
         Schema::create('domains', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->timestamp('updated_at');
             $table->timestamp('created_at');
