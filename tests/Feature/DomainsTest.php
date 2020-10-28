cad8b59dbf4dcac4a46c9c3c726ea9883db4b45a
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-10-28 05:10:27 +0300

    domains checks added Tests added

--- tests/Feature/DomainsTest.php
+++ tests/Feature/DomainsTest.php
@@ -3,11 +3,22 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class DomainsTest extends TestCase
 {
     use RefreshDatabase;
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = \Faker\Factory::create();
+        $this->seed();
+    }
 
     public function testIndex()
     {
@@ -39,6 +50,18 @@ class DomainsTest extends TestCase
         $response->assertRedirect();
 
         $this->assertDatabaseMissing('domains', $data);
+    }
+
+    public function testDomainCheckStatus()
+    {
+        $name = "https://www.google.ru/";
+        Http::fake([
+            $name => Http::response('<html>fake site</html>', 203)
+        ]);
+        $response = $this->post(route('domains.check', 1));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('domain_checks', ['status_code' => '203']);
     }
 
     public function domainNamesProvider()
