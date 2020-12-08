8de303977cd140eb421dfc3bde4ac8bdd5101119
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-12-08 14:07:37 +0300

    exception clarification + vars naming refactoring

--- app/Http/Controllers/DomainCheckController.php
+++ app/Http/Controllers/DomainCheckController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use DiDom\Document;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
@@ -11,11 +13,12 @@ class DomainCheckController extends Controller
 {
     private function buildDomainCheckData($name, $id)
     {
-            $data = Http::get($name);
-            $status = $data->status();
-            $body = $data->body();
+            $httpResponse = Http::get($name);
+            $status = $httpResponse->status();
+            $body = $httpResponse->body();
             $document = new Document($body);
-            $h1 = $document->has('h1') ? $document->first('h1')->text() : null;
+            $h1Element = $document->first('h1');
+            $h1 = optional($h1Element)->text();
             $keywordsElement = $document->first('meta[name=keywords]');
             $keywords = optional($keywordsElement)->getAttribute('content');
             $descriptionElement = $document->first('meta[name=description]');
@@ -47,8 +50,10 @@ class DomainCheckController extends Controller
             flash(
                 __('flashes.domain.checked', ['name' => $domain->name])
             )->success();
-        } catch (\Throwable $err) {
-            flash($err->getMessage())->error();
+        } catch (ConnectionException $cathedConnectionException) {
+            flash($cathedConnectionException->getMessage())->error();
+        } catch (RequestException $catchedRequestException) {
+            flash($catchedRequestException->getMessage())->error();
         }
         return redirect()
             ->route('domains.show', $id);
